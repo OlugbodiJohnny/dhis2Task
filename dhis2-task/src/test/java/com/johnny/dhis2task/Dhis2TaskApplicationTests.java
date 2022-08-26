078a7a6cb7123c0e6d15a7a6d817dbdc6419434f
Olugbodi Johnny <olugbodijohnny@live.com>
2022-08-26 02:28:42 +0100

    added dhis2-javaclient dependency to POM.xml file

---
+++ dhis2-task/src/test/java/com/johnny/dhis2task/Dhis2TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnny.dhis2task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dhis2TaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
