213612a2396ed082f15479d5905a99f2e315753b
Olugbodi Johnny <olugbodijohnny@live.com>
2022-08-29 19:48:59 +0100

    added application.properties file and setup custom configs

--- dhis2-task/src/main/java/com/johnny/dhis2task/Dhis2TaskApplication.java
+++ dhis2-task/src/main/java/com/johnny/dhis2task/Dhis2TaskApplication.java
@@ -2,6 +2,7 @@ package com.johnny.dhis2task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class Dhis2TaskApplication {
